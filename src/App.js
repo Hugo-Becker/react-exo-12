1232ab978b94c0301bc6d53b7d1de4a22dafb5ed
Hugo <becker.hugo@hotmail.com>
2021-01-15 12:49:24 +0100

    s1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react'
+import Article from './Article'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state={
+    nbr:0
+  }
+  plusUn=()=>{
+    var number=this.state.nbr
+    this.setState({
+      nbr:number+=1
+    })
+  }
+  moinsUn=()=>{
+    var number=this.state.nbr
+    this.setState({
+      nbr:number-=1,
+    })
+  }
+  
+  render(){
+    if(this.state.nbr>=2){
+      return(
+        <div>
+          <span>{this.state.nbr}</span>
+
+          <button onClick={this.plusUn}>+</button>
+          <button onClick={this.moinsUn}>-</button>
+          <Article></Article>
+          <Article></Article>
+          <Article></Article>
+        </div>
+        
+      );
+    }else{
+      return(
+        <div>
+          <span>{this.state.nbr}</span>
+          <button onClick={this.plusUn}>+</button>
+          <button onClick={this.moinsUn}>-</button>
+          <h1>Pas d'articles à afficher</h1>
+
+        </div>
+      )
+    }
+    
+
+    // return (
+    //   <div>
+    //     <Article />
+    //     <Article />
+
+    //   </div>
+      
+
+
+  
+    //   );
+
+  }
+  
 }
 
 export default App;
